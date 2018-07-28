7a6a98bc8b81e672bae9646f8c813525101b2623
Norio <umanari145@gmail.com>
2018-07-28 20:30:18 +0900

    並列処理をさらにスマートな書き方でかく

---
+++ heiretsu2.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"log"
+	"time"
+)
+
+func main() {
+	log.Print("started")
+	//https://qiita.com/suin/items/82ecb6f63ff4104d4f5d
+	sleep_finish := make(chan bool)
+
+	go func() {
+		log.Print("sleep1 start")
+		time.Sleep(1 * time.Second)
+		log.Print("sleep1 end")
+		sleep_finish <- true
+	}()
+
+	go func() {
+		log.Print("sleep2 start")
+		time.Sleep(2 * time.Second)
+		log.Print("sleep2 end")
+		sleep_finish <- true
+	}()
+
+	go func() {
+		log.Print("sleep3 start")
+		time.Sleep(3 * time.Second)
+		log.Print("sleep3 end")
+		sleep_finish <- true
+	}()
+
+	//もし一つだけチャネルだと1つが終わった時点で終了してしまう
+	//<-sleep_finish
+
+	//上記の理由からチェネル分のfinishを作る
+	for i := 1; i <= 3; i++ {
+		<-sleep_finish
+	}
+
+	log.Print("all_finished")
+}
---
+++ heiretsu3.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"log"
+	"time"
+)
+
+func main() {
+	log.Print("started")
+	//https://qiita.com/suin/items/82ecb6f63ff4104d4f5d
+	sleep_finish := make(chan bool)
+
+	sleepfuncs := []func(){
+		func() {
+			log.Print("sleep1 start")
+			time.Sleep(1 * time.Second)
+			log.Print("sleep1 end")
+			sleep_finish <- true
+		},
+		func() {
+			log.Print("sleep2 start")
+			time.Sleep(2 * time.Second)
+			log.Print("sleep2 end")
+			sleep_finish <- true
+		},
+		func() {
+			log.Print("sleep3 start")
+			time.Sleep(3 * time.Second)
+			log.Print("sleep3 end")
+			sleep_finish <- true
+		},
+	}
+
+	//for文でぶん回す
+	for _, sleepfunc := range sleepfuncs {
+		go sleepfunc()
+	}
+
+	//終わらせるチャネル
+	for i := 1; i <= 3; i++ {
+		<-sleep_finish
+	}
+
+	log.Print("all_finished")
+}
